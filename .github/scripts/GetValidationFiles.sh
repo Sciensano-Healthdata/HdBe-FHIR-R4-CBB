ad640129f5210f82a408a0dec574fb28389e303f
Liset <l.vanwijk@furore.com>
2022-03-11 13:47:57 +0100

    Validate without an output file

--- .github/scripts/GetValidationFiles.sh
+++ .github/scripts/GetValidationFiles.sh
@@ -1,15 +1,17 @@
-
-
 # Copy all files from resources, examples and terminology into a separate folder
 
 mkdir ValidationFiles
 
-cp -R resources/. ValidationFiles
-cp -R examples/. ValidationFiles
-cp -R terminology/. ValidationFiles
+cp -R resources ValidationFiles/resources
+cp -R examples ValidationFiles/examples
+cp -R terminology ValidationFiles/terminology
+
+echo "All files to validate are copied to the ValidationFiles directory."
 
 # Go to the folder 
 cd ValidationFiles
 
-# Delete all files with an *.md extension. This could be elaborated if there occur other issues.
+# Delete all files with an *.md extension as they are not accepted by the Java validator. 
+# This could be elaborated if there occur other issues.
 find . -name "*.md" -type f -delete
+echo "All *.md files are deleted from the ValidationFiles"
