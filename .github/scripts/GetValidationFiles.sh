877d23db421456e7051252bf28aee578543d40f1
Ardon Toonstra <ardon@fire.ly>
2022-03-17 17:07:52 +0100

    Textual improvements and potential fix

--- .github/scripts/GetValidationFiles.sh
+++ .github/scripts/GetValidationFiles.sh
@@ -1,10 +1,11 @@
-# Copy all files from resources, examples and terminology into a separate folder
+# Copy all files from resources, examples and terminology into a separate folder.
 
 mkdir ValidationFiles
 
+# Copy all files recursive into new folder.
 cp -R resources ValidationFiles/resources
 cp -R examples ValidationFiles/examples
-cp -R terminology ValidationFiles/terminology
+cp -R terminology ValidationFiles/resources
 
 echo "All files to validate are copied to the ValidationFiles directory."
 
