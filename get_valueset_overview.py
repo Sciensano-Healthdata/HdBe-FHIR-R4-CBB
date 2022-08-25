922847261ea1aac9a472e5a478768f205bfa6bed
Liset <liset@fire.ly>
2022-08-25 10:30:37 +0200

    #252 Overview generator of concepts in valuesets, including an overview file

--- get_valueset_overview.py
+++ get_valueset_overview.py
@@ -12,58 +12,51 @@ from datetime import datetime
 input_folder = 'terminology/'
 output_folder = "terminology/"
 
-
-# Features
+#Make headers for .csv file
+with open(output_folder + 'codelist_overview.csv', 'w', encoding='utf8') as h:
+    h.write('CODE_LIST_TYPE;CODE_INDEX;CODE_VALUE;CODE_CONTENT_ID;LABEL_EN;LABEL_NL;LABEL_FR;CODE_SYSTEM' + '\n')
+    h.close()
 
 # Generate an overview of all ValueSets
 def make_code_overview(message):
     """ writes message to the log file """
-    with open(output_folder + '_overview.txt', 'a', encoding='utf8') as f:
-        now = str(datetime.now())
+    with open(output_folder + 'codelist_overview.csv', 'a', encoding='utf8') as f:
         message = message + '\n' 
         f.write(message)
         f.close()
 
-
-# For each Value in a ValueSet:
-# code
-# display
-# designation nl-BE
-# designation nl-FR
-def addDesignations(valueset, filename):
-    vsci = []
+# For each concept in a ValueSet:
+def createConceptOverview(valueset, filename):
     for x in valueset.compose.include:
-        vs = (valueset.id)
+        #vs = (valueset.id)
         #Only get designations for SNOMED codes, that are extententionally defined (no filter) and do have a concept (TypeOfLivingWill contains all of SNOMED).
-        if x.system == 'http://snomed.info/sct' and x.valueSet is None and x.filter is None and x.concept is not None: 
+        if x.valueSet is None and x.filter is None and x.concept is not None: 
+            index = 0
+            #system = x.system
             for x2 in x.concept:
                 con = ValueSetComposeIncludeConcept.parse_obj(x2)
-                concepts = []
-                #Add name of the valueset (All capitalized )
-                concepts.append(vs.upper())
-                # Add code en display 
-                concepts.append(con.code)
-                concepts.append(con.display)
-                # TO DO: ADD a counter for the concepts in each valueSet
+                concepts = ''
+                index += 1
+                #Add name of the valueset (All capitalized), the indexnumber, code en display  (double ;; are for CODE_CONTENT_ID that we do not fill)
+                concepts += valueset.id.upper() + ';' + str(index) + ';' + con.code + ';;' + con.display + ';'
                 nl_display = ''
                 fr_display = ''
                 #Add Franse en NL display (should still deal with non existing translations when fitting in an csv file)
                 if con.designation is not None:
                     for x3 in con.designation:
-                        con2 = ValueSetComposeIncludeConceptDesignation.parse_obj(x3)
-                        if con2.language == 'nl-BE':
+                        des = ValueSetComposeIncludeConceptDesignation.parse_obj(x3)
+                        if des.language == 'nl-BE':
                                 nl_display = x3.value
-                        if con2.language == 'fr-BE':
+                        if des.language == 'fr-BE':
                                 fr_display = x3.value 
 
-                #WIP: Now only printed, should be added to a file neatly.
-                concepts.append(nl_display)
-                concepts.append(fr_display)                   
-                print(concepts)
-                # Writes to file (nu nog alles aan elkaar)
-                # Nog alternatief voor lege nl en fr displays
-                #make_code_overview(str(con.code + con.display + nl_display + fr_display))   
+                #Add the Dutch and French display, and the system of the code:
+                concepts += nl_display + ';' + fr_display + ';' + x.system
 
+                # Writes concepts to file (nu nog alles aan elkaar)
+                make_code_overview(concepts)   
+
+        
 # Nice to have
 # If a CodeSystem is included as a whole (and is not SNOMED)
 # --> Go through that system and get
@@ -72,22 +65,12 @@ def addDesignations(valueset, filename):
 # designation nl-BE
 # designation nl-FR
 
-
-
-
-
-
-# Get all files from the input folder that meet the filename creteria. 
-# Next, add EN/NL/FR designations for all extensionally defined SNOMED codes and write the updated files to the output folder. 
+# Get all files from the input folder that meet the filename criteria. 
+# Next, for each valueset, find the concepts and create an overview of the codes in that valueset, including Dutch and French display 
+# Write all an overview containing al concepts to the output folder. 
 file = Path(input_folder).glob('ValueSet-*.xml')
-print(file)
-print(input_folder)
 for f in file:
     filename = os.path.basename(f)
-    valueset = addDesignations(ValueSet.parse_file(f), filename)
-      #  xml_str = valueset.xml(pretty_print=True)
-  #  output_file = open(output_folder + filename, 'w', encoding='UTF8') 
-  #  output_file.write(xml_str)
-  #  output_file.close()
+    createConceptOverview(ValueSet.parse_file(f), filename)
 
-print('Finished getting designations')
+print('Finished getting overview')
